ed5a62e030915df858129e07b8c011cf37fc8723
Marie57412 <mariebraun1@outlook.fr>
2023-06-16 11:00:49 +0200

    maj filtres et ajout event

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,54 +1,72 @@
+let worksData, categoriesData;
+
+/* recupération des données par l'api */
 fetch('http://localhost:5678/api/works')
   .then(response => response.json())
-  .then(data => {                                               //recup donné api gallery
-    array(data);
+  .then(data => {
+    worksData = data;
+    afficherGallery(data);
     console.log(data[0]);
+    return fetch('http://localhost:5678/api/categories');
+  })
+  .then(response => response.json())
+  .then(category => {
+    categoriesData = category;
+    filters(category);
+    console.log(category[0]);
   })
   .catch(error => {
-    console.error('Une erreur s\'est produite :', error);
+    console.error('Une erreur s\'est produite', error);
   });
-    
-  fetch('http://localhost:5678/api/categories')
-    .then(response => response.json())
-    .then(category => {                                            // recup donné category
-      filters(category);
-      console.log(category[i]);
-    })
-    .catch(error => {
-      console.error('Une erreur s\'est produite', error);
-    });
 
-//generer la galérie 
-function array (data){
-  for(i = 0; i < data.length; i++)
+/* fonction qui permet affiche la gallery */
+function afficherGallery(data){
+  for(i =0 ; i < data.length ; i++)
   {
-    const gallery = document.querySelector(".gallery");
-    const figure = document.createElement("figure");
-    const picture = document.createElement("img");
-    picture.src = data[i].imageUrl;
-    const name = document.createElement("p");
-    name.innerText = data[i].title;
+    const gallery = document.querySelector('.gallery');
 
-    gallery.appendChild(figure)
-    figure.appendChild(picture)
-    figure.appendChild(name)
+    const figure  = document.createElement("figure");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = data[i].imageUrl;
+
+    const nomElement = document.createElement("p");
+    nomElement.innerText = data[i].title;
+
+    gallery.appendChild(figure);
+    figure.appendChild(imageElement);
+    figure.appendChild(nomElement);
   }
 }
 
-//générer les boutons filtres
 function filters (category){
-
   category.unshift({ name: "Tous" });
 
   for(i = 0; i < category.length; i++)
   {
-    const filterbtn = document.querySelector(".filters-btn")
+    const filterbtn = document.querySelector(".filters-btn");
     const filter = document.createElement("button");
     filter.innerText = category[i].name;
-    
+
     filterbtn.appendChild(filter);
   }
-  
+
+  const filterBtnEvent = document.querySelectorAll(".filters-btn button");
+
+  filterBtnEvent.forEach(button => {
+    button.addEventListener("click", function() {
+      // Efface le contenu de la galerie
+      document.querySelector(".gallery").innerHTML = '';
+
+      // Filtrage des projets en fonction de la catégorie sélectionnée
+      let filteredData = worksData.filter((item) => {
+        return item.category.name === button.textContent || button.textContent === "Tous";
+      });
+
+      console.log(filteredData);
+
+      // Utilisation des projets filtrés pour afficher la galerie mise à jour
+      afficherGallery(filteredData);
+    });
+  });
 }
-
-
